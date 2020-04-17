9bf46ab0e44da27d5a6c952df263e137a5580ef9
reno <renan.modenese@gmail.com>
2020-04-17 16:41:07 -0300

    Incluindo post delete

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -41,4 +41,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
